d192f3bf647e0375fbe329aa6dc35735b6f32522
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-09 13:32:10 +0300

    Replacing void on String in method writeFile. Add method String writeFile(String myName)

--- src/app/FileWriteService.java
+++ src/app/FileWriteService.java
@@ -5,23 +5,27 @@ import java.io.IOException;
 
 public class FileWriteService {
 
-    public static void writeFile(String myName, String myText){
+    public static String writeFile(String myName, String myText){
+
+        try {
+            FileOutputStream file = new FileOutputStream(writeFile(myName));
+            byte [ ]  byArr = myText.getBytes();
+            file.write(byArr, 0, byArr.length);
+            System.out.println("Success!");
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+                             }
+        return myName;
+    }
+    public static String writeFile(String myName){
         String myFile;
         if(myName.isEmpty()){
             System.out.println("Invalid file name!");
-            return;
-        }else {
-         myFile = Constants.BASE_PATH_IN + myName + ".txt";
+             return myName;
+                    }else {
+            myFile = Constants.BASE_PATH_IN + myName + ".txt";
         }
-        try {
-            FileOutputStream file = new FileOutputStream(myFile);
-            byte [ ]  byArr = myText.getBytes();
-            file.write(byArr, 0, byArr.length);
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
-           System.exit(0);
-                    }
-        System.out.println("Success!");
+        return myFile;
     }
 
 }
