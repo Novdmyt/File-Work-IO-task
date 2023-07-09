95fcc55e3c1f79f2c2f82fc8db20861c6de4a193
Novdmyt <135864604+Novdmyt@users.noreply.github.com>
2023-07-09 13:30:49 +0300

    Replacing void on String in method fileRead. Fix cod

--- src/app/FileReadService.java
+++ src/app/FileReadService.java
@@ -4,20 +4,21 @@ import utils.Constants;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 public class FileReadService {
-public static void fileRead(String myName){
+public static String  fileRead(String myName) {
     String myFile = Constants.BASE_PATH_IN + myName + ".txt";
 
+   String nextString = " ";
     try {
         BufferedReader br = new BufferedReader(new InputStreamReader
                 (new FileInputStream(myFile),
                         StandardCharsets.UTF_8));
-        String nextString;
-        while ((nextString = br.readLine()) != null){
+        while ((nextString = br.readLine()) != null) {
             System.out.println(nextString);
         }
-    } catch (IOException ex){
-               System.out.println(ex.getMessage());
+    } catch (IOException ex) {
+        System.out.println(ex.getMessage());
     }
+    return nextString;
 
 }
 }
